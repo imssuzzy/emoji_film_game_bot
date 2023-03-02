b8f40020b8fbe71df911d61f70ed1f058b570f81
imssuzzy <sezim.cholponkulova@gmail.com>
2023-03-02 14:02:01 +0600

    add inline markup

--- bot_utils/handlers.py
+++ bot_utils/handlers.py
@@ -6,8 +6,11 @@ async def welcome_message(message:types.Message):
     Привет! Давай поиграем в 'Угадай фильм по эмодзи!'. Чтобы начать игру, отправь мне сообщение 'Начать игру'.🎮🎬
     """
     categories = CategoryManager().get_all_categories()
+    markup = types.InlineKeyboardMarkup(width=1)
     for c in categories:
-        await message.answer(c.name)
+        markup.add(
+            types.InlineKeyboardButton(c.name, callback_data=f"category_{c.id}")
+        )
     
-    await message.answer(text)
+    await message.answer(text, reply_markup=markup)
 
