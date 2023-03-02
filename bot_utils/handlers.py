5bc6130be29fc6d73dd1edae9360711ed8f2d451
imssuzzy <sezim.cholponkulova@gmail.com>
2023-03-02 13:55:36 +0600

    add categories

--- bot_utils/handlers.py
+++ bot_utils/handlers.py
@@ -1,9 +1,13 @@
 from aiogram import types
-
+from database.manager import CategoryManager
 
 async def welcome_message(message:types.Message):
     text = """
     Привет! Давай поиграем в 'Угадай фильм по эмодзи!'. Чтобы начать игру, отправь мне сообщение 'Начать игру'.🎮🎬
     """
+    categories = CategoryManager().get_all_categories()
+    for c in categories:
+        await message.answer(c.name)
+    
     await message.answer(text)
 
